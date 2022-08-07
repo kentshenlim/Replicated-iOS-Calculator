f59bc1a0a2cfb06e57f0a7aeaffb3aec97267720
LessSmartKent <kentshen_lim24@hotmail.com>
2022-08-07 18:40:40 +0100

    Store operator in array and digits as a string

--- script.js
+++ script.js
@@ -30,10 +30,22 @@ function updateDisplay(number) {
 
 
 let currentScreen = "";
+let currentNumber = "";
+let currentOperator = [];
 digits = document.querySelectorAll("button.digit");
 digits.forEach(digit => {
     digit.addEventListener("click", () => {
+        currentNumber += digit.textContent;
         currentScreen += digit.textContent;
+        updateDisplay(currentScreen);
+    })
+})
+
+operators = document.querySelectorAll("button.operator");
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        currentOperator.push(operator.textContent);
+        currentScreen = "";
         updateDisplay(currentScreen);
     })
 })
