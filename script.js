d0efc97d71fa479b0cb715c7656c67c61be9d5fa
LessSmartKent <kentshen_lim24@hotmail.com>
2022-08-08 22:03:05 +0100

    Remove bug due to clearing array by evaluateNow

--- script.js
+++ script.js
@@ -29,6 +29,12 @@ function updateDisplay(number) {
 }
 
 
+function clearScreen() {
+    currentScreen = "";
+    updateDisplay(currentScreen);
+}
+
+
 let currentScreen = "";
 let currentNumber = "";
 let operatorArray = [];
@@ -41,34 +47,36 @@ const digits = document.querySelectorAll("button.digit"); // 0-9 and period
 digits.forEach(digit => {
     digit.addEventListener("click", () => {
         if (digitNextClear) {
-            currentScreen = "";
-            updateDisplay(currentScreen);
-            digitNextClear = false; // Digits after operator will clear screen
+            updateDisplay(currentNumber); // Clear screen, current number is ""
+            digitNextClear = false; // Only digit just after operator will clear screen
         }
         currentNumber += digit.textContent;
-        currentScreen = currentNumber;
-        updateDisplay(currentScreen);
+        updateDisplay(currentNumber);
     })
 })
 
 const operators = document.querySelectorAll("button.operator"); // + - x /
 operators.forEach(operator => {
     operator.addEventListener("click", () => {
-        let clicked = operator.textContent;
         numberArray.push(+currentNumber); // Store digit string as number in array
         currentNumber = ""; // Reset to empty string for future use
-        digitNextClear = true; // After clicked, next digit will clear screen
+        digitNextClear = true; // After operator, next digit will clear screen
+        const clicked = operator.textContent;
         operatorArray.push(clicked);
         if (evaluateNext) {
             evaluateNow();
-            evaluateNext = false;
+            evaluateNext = false; // Instant display of multiplication and division
+            operatorArray.push(clicked); // Stored again, cleared by evaluateNow()
         }
-        evaluateNext = (clicked == '×' || clicked == '÷');
+        evaluateNext = (clicked == '×' || clicked == '÷'); // Preparing instant display
     })
 })
 
 const equal = document.querySelector("#equal");
-equal.addEventListener("click", evaluateNow);
+equal.addEventListener("click", () => {
+    evaluateNow();
+    numberArray = [];
+});
 
 function evaluateNow() {
     numberArray.push(+currentNumber);
@@ -93,5 +101,5 @@ function evaluateNow() {
     } 
     updateDisplay(ans);
     operatorArray = [];
-    numberArray = [];
+    numberArray = [ans];
 }
