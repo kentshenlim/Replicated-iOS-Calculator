a3301b67a807a5776aea1027dc75f2627a0a1a4e
LessSmartKent <kentshen_lim24@hotmail.com>
2022-08-08 21:35:03 +0100

    Add instant evaluation for multiplication and division

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ let operatorArray = [];
 let numberArray = [];
 let digitNextClear = false; 
 let ans;
+let evaluateNext = false;
 
 const digits = document.querySelectorAll("button.digit"); // 0-9 and period
 digits.forEach(digit => {
@@ -53,15 +54,23 @@ digits.forEach(digit => {
 const operators = document.querySelectorAll("button.operator"); // + - x /
 operators.forEach(operator => {
     operator.addEventListener("click", () => {
+        let clicked = operator.textContent;
         numberArray.push(+currentNumber); // Store digit string as number in array
         currentNumber = ""; // Reset to empty string for future use
         digitNextClear = true; // After clicked, next digit will clear screen
-        operatorArray.push(operator.textContent);
+        operatorArray.push(clicked);
+        if (evaluateNext) {
+            evaluateNow();
+            evaluateNext = false;
+        }
+        evaluateNext = (clicked == '×' || clicked == '÷');
     })
 })
 
 const equal = document.querySelector("#equal");
-equal.addEventListener("click", () => {
+equal.addEventListener("click", evaluateNow);
+
+function evaluateNow() {
     numberArray.push(+currentNumber);
     currentNumber = "";
     digitNextClear = true;
@@ -85,6 +94,4 @@ equal.addEventListener("click", () => {
     updateDisplay(ans);
     operatorArray = [];
     numberArray = [];
-})
-
-
+}
